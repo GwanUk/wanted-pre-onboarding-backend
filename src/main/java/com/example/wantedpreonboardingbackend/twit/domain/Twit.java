48cd859c49fba6b92607ada1a577f83b603f50ab
gwanuk <guk1502012@gmail.com>
2023-08-13 15:13:21 +0900

    FEAT: Add twit domain

---
+++ src/main/java/com/example/wantedpreonboardingbackend/twit/domain/Twit.java
@@ -0,0 +1,27 @@
+package com.example.wantedpreonboardingbackend.twit.domain;
+
+import com.example.wantedpreonboardingbackend.mebmer.domain.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "TWIT")
+public class Twit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TWIT_ID")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+}
